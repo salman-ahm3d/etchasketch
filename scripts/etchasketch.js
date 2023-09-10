01d3211669be0d0127a760990b3f1258560d0530
salman-ahm3d <salmanahmed69890@gmail.com>
2023-09-10 02:15:57 -0600

    Make footer sticky and black the default color

--- scripts/etchasketch.js
+++ scripts/etchasketch.js
@@ -81,5 +81,12 @@ colorPickerBtn.addEventListener('change', () => {
 })
 
 
+jscolor.presets.default = {
+    value: '#000000',
+    position: 'right',
+    backgroundColor: '#333',
+    palette: '#fff #000 #808080 #996e36 #f55525 #ffe438 #88dd20 #22e0cd #269aff #bb1cd4',
+};
+
 
 colorOnClick();
